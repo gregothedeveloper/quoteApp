1121aedac09bdd0b6dff0aee57c4f4da20a2a8b3
Brian Ochieng <ochiengbrian634@gmail.com>
2024-04-21 11:02:32 +0300

    font modification

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 const api_url = "https://api.quotable.io/random";
+const shareButton = document.getElementById("shareButton");
 async function getQuote(url) {
   const response = await fetch(url);
   var data = await response.json();
@@ -16,3 +17,11 @@ function tweet() {
     "width=600, height=700"
   );
 }
+shareButton.addEventListener("click", () => {
+  const shareText = "Check out this awesome quote app ❤️🔥!";
+  const shareUrl = "https://quote-app-orpin.vercel.app/";
+  const whatsappUrl = `whatsapp://send?text=${encodeURIComponent(
+    shareText
+  )}%20${encodeURIComponent(shareUrl)}`;
+  window.location.href = whatsappUrl;
+});
